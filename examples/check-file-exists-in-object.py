07a2445adaf0e2aabc381524314648855f41c80c
Michael Kistler <michael.kistler@istb.unibe.ch>
2016-02-26 14:06:20 +0100

    inital commit for file hash check example

---
+++ examples/check-file-exists-in-object.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+"""
+=======
+INFOS
+=======
+* Example on how to check if a local file is contained in an object online. Adapt the credential, the local file an the object id
+* python version: 3 
+* @author: Michael Kistler 2016
+
+========
+CHANGES
+========
+* initial version base ond v0.8.1
+
+"""
+
+import connectVSD
+import os
+from pathlib import Path, PurePath, WindowsPath
+
+
+## connect using credentials
+api=connectVSD.VSDConnecter(url='https://demo.virtualskeleton.ch/api/', username= "demo@virtualskeleton.ch", password = "demo")
+
+## specify the local file
+fp = Path('D:' + os.sep, 'hash','vsd_file_926373_20160226_120352.stl')
+
+## which object to check
+obj = api.getObject(1)
+
+## the check
+if api.checkFileInObject(obj, fp):
+    print('found')
+
+else:
+    print('not found')
